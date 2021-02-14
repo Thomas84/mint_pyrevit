f043edd29b667f39d49315bbfca119baca856888
Mengfan Lou <mengfan.lou@gmail.com>
2021-02-13 22:08:15 -0500

    bug fix

--- CustomExtension.extension/Mint.tab/Modify.panel/Modify1.stack3/Rename.pulldown/Batch Rename Families.pushbutton/script.py
+++ CustomExtension.extension/Mint.tab/Modify.panel/Modify1.stack3/Rename.pulldown/Batch Rename Families.pushbutton/script.py
@@ -14,29 +14,19 @@ sys.path.append(syspath1)
 # Built Path
 syspath2 = config.get('SysDir','SecondaryPackage')
 sys.path.append(syspath2)
-import Selection
 clr.AddReference('System')
 from Autodesk.Revit.DB import Document, FilteredElementCollector, GraphicsStyle, Transaction, BuiltInCategory,\
     RevitLinkInstance, UV, XYZ, SpatialElementBoundaryOptions, CurveArray, ElementId, View, RevitLinkType, WorksetTable,\
     Workset, FilteredWorksetCollector, WorksetKind, RevitLinkType, RevitLinkInstance, View3D, ViewType,ElementClassFilter,\
     ViewFamilyType, ViewFamily, BuiltInParameter, IndependentTag, Reference, TagMode, TagOrientation, IFamilyLoadOptions,\
     FamilySymbol
-from System import EventHandler, Uri
-from Autodesk.Revit.UI.Events import ViewActivatedEventArgs, ViewActivatingEventArgs
 from pyrevit import revit, DB, forms
 clr. AddReferenceByPartialName('PresentationCore')
 clr.AddReferenceByPartialName('PresentationFramework')
 clr.AddReferenceByPartialName('System.Windows.Forms')
-import System.Windows.Forms
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
 from pyrevit import framework
-from pyrevit import script
-from pyrevit import DB, UI
-import datetime, os
-from pyrevit import HOST_APP, framework
-import Autodesk.Revit.DB.ExtensibleStorage
-import uuid
 import shutil
 
 __doc__ = 'To be run in a Revit Model. ' \
@@ -137,68 +127,68 @@ class FamilyOption(IFamilyLoadOptions):
 		return True
 
 
-specSections = {"Air Terminals":"23",
-"Cable Tray Fittings":"26",
-"Cable Tray Runs":"26",
-"Cable Trays":"26",
-"Casework":"12",
-"Columns":"06",
-"Communication Devices":"27",
-"Conduit Fittings":"26",
-"Conduit Runs":"26",
-"Conduits":"26",
-"Curtain Grids":"08",
-"Curtain Panels":"08",
-"Curtain Systems":"08",
-"Curtain Wall Mullions":"07",
-"Data Devices":"27",
-"Doors":"08",
-"Duct Accessories":"23",
-"Duct Fittings":"23",
-"Duct Insulations":"23",
-"Duct Linings":"23",
-"Duct Placeholders":"23",
-"Duct Systems":"23",
-"Ducts":"23",
-"Electrical Circuits":"26",
-"Electrical Equipment":"26",
-"Electrical Fixtures":"26",
-"Electrical Spare/Space Circuits":"26",
-"Entourage":"12",
-"Fire Alarm Devices":"28",
-"Flex Ducts":"23",
-"Flex Pipes":"22",
-"Floors":"03",
-"Furniture":"12",
-"Furniture Systems":"12",
-"Generic Models":"00",
-"HVAC Zones":"23",
-"Lighting Devices":"26",
-"Lighting Fixtures":"26",
-"Mass":"00",
-"Mechanical Equipment":"23",
-"Mechanical Equipment Set Boundary Lines":"23",
-"Mechanical Equipment Sets":"23",
-"MEP Fabrication Containment":"23",
-"MEP Fabrication Ductwork":"23",
-"MEP Fabrication Hangers":"23",
-"MEP Fabrication Pipework":"23",
-"Nurse Call Devices":"27",
-"Pipe Fittings":"22",
-"Pipe Insulations":"22",
-"Pipe Placeholders":"22",
-"Pipe Segments":"22",
-"Pipes":"22",
-"Piping Systems":"22",
-"Planting":"12",
-"Plumbing Fixtures":"22",
-"Security Devices":"27",
-"Site":"12",
-"Specialty Equipment":"11",
-"Sprinklers":"21",
-"Telephone Devices":"27",
-"Topography":"31",
-"Windows":"08"}
+specSections = {"Air Terminals": "23",
+"Cable Tray Fittings": "26",
+"Cable Tray Runs": "26",
+"Cable Trays": "26",
+"Casework": "12",
+"Columns": "06",
+"Communication Devices": "27",
+"Conduit Fittings": "26",
+"Conduit Runs": "26",
+"Conduits": "26",
+"Curtain Grids": "08",
+"Curtain Panels": "08",
+"Curtain Systems": "08",
+"Curtain Wall Mullions": "07",
+"Data Devices": "27",
+"Doors": "08",
+"Duct Accessories": "23",
+"Duct Fittings": "23",
+"Duct Insulations": "23",
+"Duct Linings": "23",
+"Duct Placeholders": "23",
+"Duct Systems": "23",
+"Ducts": "23",
+"Electrical Circuits": "26",
+"Electrical Equipment": "26",
+"Electrical Fixtures": "26",
+"Electrical Spare/Space Circuits": "26",
+"Entourage": "12",
+"Fire Alarm Devices": "28",
+"Flex Ducts": "23",
+"Flex Pipes": "22",
+"Floors": "03",
+"Furniture": "12",
+"Furniture Systems": "12",
+"Generic Models": "00",
+"HVAC Zones": "23",
+"Lighting Devices": "26",
+"Lighting Fixtures": "26",
+"Mass": "00",
+"Mechanical Equipment": "23",
+"Mechanical Equipment Set Boundary Lines": "23",
+"Mechanical Equipment Sets": "23",
+"MEP Fabrication Containment": "23",
+"MEP Fabrication Ductwork": "23",
+"MEP Fabrication Hangers": "23",
+"MEP Fabrication Pipework": "23",
+"Nurse Call Devices": "27",
+"Pipe Fittings": "22",
+"Pipe Insulations": "22",
+"Pipe Placeholders": "22",
+"Pipe Segments": "22",
+"Pipes": "22",
+"Piping Systems": "22",
+"Planting": "12",
+"Plumbing Fixtures": "22",
+"Security Devices": "27",
+"Site": "12",
+"Specialty Equipment": "11",
+"Sprinklers": "21",
+"Telephone Devices": "27",
+"Topography": "31",
+"Windows": "08"}
 
 specExceptions = ["Railings", "Structural", "Supports", "Terminations"]
 
@@ -254,7 +244,15 @@ for familyName in families.keys():
     if len(familyName.split("-")) >= 3 and \
             familyRegex.findall(familyName) >= 1 and \
             cate.upper() == familyName.split("-")[1]:
-        pass
+
+        src_fpath = onlyfiles[familyName]
+        dest_fpath = onlyfiles[familyName].replace(folderPath, destinationFolderPath)
+
+        try:
+            shutil.copy(src_fpath, dest_fpath)
+        except IOError as io_err:
+            os.makedirs(os.path.dirname(dest_fpath))
+            shutil.copy(src_fpath, dest_fpath)
     else:
         if not "Railings" in cate and not"Structural" in cate and not "Supports" in cate and not "Terminations" in cate:
             try:
